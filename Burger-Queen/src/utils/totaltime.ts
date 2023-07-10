3742ed9dca3c79320ce5a3cea12b926e81c94a34
danipreciado <danielapca96@gmail.com>
2023-07-10 16:34:58 -0700

    feature for filtering orders per status

---
+++ Burger-Queen/src/utils/totaltime.ts
@@ -0,0 +1,19 @@
+const calculateTotalPreparationTime = (dataEntry: string, dataDelivering: string) => {
+    const entryTime = new Date(dataEntry).getTime();
+    const deliveringTime = new Date(dataDelivering).getTime();
+    const totalTime = deliveringTime - entryTime;
+
+    const hours = Math.floor(totalTime / (1000 * 60 * 60));
+    const minutes = Math.floor((totalTime % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((totalTime % (1000 * 60)) / 1000);
+
+    let totalTimeStr = "";
+    if (hours > 0) {
+      totalTimeStr += `${hours}h `;
+    }
+    totalTimeStr += `${minutes}m ${seconds}s`;
+
+    return totalTimeStr;
+  };
+
+  export default calculateTotalPreparationTime;
