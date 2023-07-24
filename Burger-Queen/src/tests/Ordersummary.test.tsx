d3fa5a8ec8f7267a41c8002b1111087b0e266a1f
Angy Rincón <rinconmezangelica@gmail.com>
2023-07-24 09:32:34 -0600

    chef btn style & ordersummary test

--- Burger-Queen/src/tests/Ordersummary.test.tsx
+++ Burger-Queen/src/tests/Ordersummary.test.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render,screen, waitFor, fireEvent} from "@testing-library/react";
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
 import Ordersummary, {
   OrdersummaryProps,
 } from "../components/ordersummary/ordersummary";
@@ -8,8 +8,7 @@ const product = {
   id: 3,
   name: "test",
   price: 500,
-  image:
-    "https://img.com/test.png",
+  image: "https://img.com/test.png",
   type: "Almuerzo",
   dateEntry: "2022-03-05 15:14:10",
   quantity: 1,
@@ -18,57 +17,55 @@ const product = {
 //   Product: jest.fn(),
 // }));
 // describe("Ordersummary", () => {
-  const mockProps: OrdersummaryProps = {
-    selectedProducts:[product],
-    onRemoveItem:jest.fn(), 
-    clearOrder: jest.fn(),
-    createOrder:jest.fn(),
-  };
+const mockProps: OrdersummaryProps = {
+  selectedProducts: [product],
+  onRemoveItem: jest.fn(),
+  clearOrder: jest.fn(),
+  createOrder: jest.fn(),
+};
 
-  it("selectesproduct renders correctly", async () => {
-   
-    render(<Ordersummary {...mockProps} />);
-    await waitFor(() => {
-      expect(screen.getByText("test")).toBeInTheDocument();
-    });
+it("selectesproduct renders correctly", async () => {
+  render(<Ordersummary {...mockProps} />);
+  await waitFor(() => {
+    expect(screen.getByText("test")).toBeInTheDocument();
   });
-  it("increasequantity return value is shown in p element", async () => {
-    render(<Ordersummary {...mockProps} />);
+});
+it("increasequantity return value is shown in p element", async () => {
+  render(<Ordersummary {...mockProps} />);
 
-    await waitFor(() => {
-      const btnmore = screen.getByAltText("añadir");
-      const btnless = screen.getByAltText("disminuir");
-      fireEvent.click(btnmore);
-      fireEvent.click(btnmore);
-      fireEvent.click(btnmore);
-      fireEvent.click(btnless);
-      expect(screen.getByTestId("p_quantity3")).toHaveTextContent("3");
-    });
-    
+  await waitFor(() => {
+    const btnmore = screen.getByAltText("añadir");
+    const btnless = screen.getByAltText("disminuir");
+    fireEvent.click(btnmore);
+    fireEvent.click(btnmore);
+    fireEvent.click(btnmore);
+    fireEvent.click(btnless);
+    expect(screen.getByTestId("p_quantity3")).toHaveTextContent("3");
   });
-  it("onRemoveItem function is called", async () => {
-    render(<Ordersummary {...mockProps} />);
-    
-    await waitFor(() => {
-      fireEvent.click(screen.getByAltText('eliminarProducto'));
-      expect(mockProps.onRemoveItem(product.id));
-    });
-    });
+});
+it("onRemoveItem function is called", async () => {
+  render(<Ordersummary {...mockProps} />);
 
-  test('handleCreateOrder function is called', async() => {
-    const createOrder= jest.fn();
-      render(<Ordersummary {...mockProps}/>);
-      await waitFor(() => {
-        const input = screen.getByTestId('clientName');
-        input.innerHTML = "Hi";
-        expect(createOrder).toHaveBeenCalled;
-      })
+  await waitFor(() => {
+    fireEvent.click(screen.getByAltText("eliminarProducto"));
+    expect(mockProps.onRemoveItem(product.id));
   });
+});
 
-  test('should call onRemoveItem when the delete button is clicked', async() => {
-      render(<Ordersummary {...mockProps}/>);
-      await waitFor(() => {
-        fireEvent.click(screen.getByText('Borrar Orden'));
-        expect(mockProps.clearOrder).toHaveBeenCalled;
-      })
+test("handleCreateOrder function is called", async () => {
+  const createOrder = jest.fn();
+  render(<Ordersummary {...mockProps} />);
+  await waitFor(() => {
+    const input = screen.getByTestId("clientName");
+    input.innerHTML = "Hi";
+    expect(createOrder).toHaveBeenCalled;
   });
+});
+
+test("should call onRemoveItem when the delete button is clicked", async () => {
+  render(<Ordersummary {...mockProps} />);
+  await waitFor(() => {
+    fireEvent.click(screen.getByText("Borrar Orden"));
+    expect(mockProps.clearOrder).toHaveBeenCalled;
+  });
+});
