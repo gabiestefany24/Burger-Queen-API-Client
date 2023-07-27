a5b35ceaf3c000e124b67e7bcffcd235ea0926be
Angy Rincón <rinconmezangelica@gmail.com>
2023-07-27 11:05:20 -0600

    uploaded manifest start_url

--- Burger-Queen/dev-dist/sw.js
+++ Burger-Queen/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.u4cpedse29g"
+    "revision": "0.hv82u9c7uf8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
